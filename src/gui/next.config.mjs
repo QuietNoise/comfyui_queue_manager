9bdd06c43e0d235721e750c58b643b003ed97815
QuietNoise <contact@jakubwang.com>
2025-05-08 22:30:57 +0100

    Disabled filename hashing for production build.

--- src/gui/next.config.mjs
+++ src/gui/next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = (envo === 'prod') ? {
     distDir: '../../web/.gui',
     basePath: "/extensions/comfyui_queue_manager/.gui",
     assetPrefix: "/extensions/comfyui_queue_manager/.gui",
+    async generateBuildId() {
+      // any deterministic string you like
+      return "prod";          // will output _next/static/prod/…
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        //  main runtime <buildId>.js  →  runtime.js
+        config.output.filename      = 'static/chunks/[name].js';
+        //  split chunks <name>.<hash>.js → <name>.js
+        config.output.chunkFilename = 'static/chunks/[name].js';
+      }
+      return config;
+    },
 } : {
     distDir: 'dist-dev',
 };
