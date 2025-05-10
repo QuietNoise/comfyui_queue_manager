fba3f7d69a156a27c3852344491315a3f8031969
QuietNoise <contact@jakubwang.com>
2025-05-10 18:43:35 +0100

    Queue list flicker fix;

--- src/gui/components/Queue.js
+++ src/gui/components/Queue.js
@@ -1,45 +1,49 @@
 "use client";           // (keep for app-router; harmless in pages-router)
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 
 // take items from parent component
 export default function Queue( { data, isLoading, error }) {
-  console.log('Queue data: ', data, isLoading, error);
-
-  if (error)       return <p className="text-red-500">Queue load failed: {error.message}</p>;
-  if (isLoading)   return <p>Loading queue…</p>;
-  if (!data || (!data.running && !data.pending)) return <p className="italic">Queue is empty.</p>;
-
-  // optional: sort by `number` so earliest show first
-  const pending = data.pending ? [...data.pending].sort((a, b) => a[0] - b[0]) : [];
-  const running = data.running ? [...data.running].sort((a, b) => a[0] - b[0]) : [];
+  const [state, setState] = useState({
+    pending:[],
+    running:[],
+  })
 
   function QueueItemRow({item}) {
     return (
       <tr className="odd:bg-neutral-900">
         <td className="px-3 py-1">{item[0]}</td>
-        <td className="px-3 py-1 break-all">{item[1]}</td>
-        <td className="px-3 py-1 text-center">{item[3].extra_pnginfo.workflow.id}</td>
+        <td className="px-3 py-1 text-center">{item[3].extra_pnginfo.workflow.workflow_name}</td>
       </tr>
     );
   }
 
+  useEffect(function () {
+    console.log("Data changed: ", data);
+    if (!data) return;
+    setState({
+      pending:data.pending ? [...data.pending].sort((a, b) => a[0] - b[0]) : [],
+      running:data.running ? [...data.running].sort((a, b) => a[0] - b[0]) : [],
+    });
+  }, [data]);
+
+  if (error)       return <p className="text-red-500">Queue load failed: {error.message}</p>;
+  if (!data || (!data.running.length && !data.pending.length)) return <p className="italic">Queue is empty.</p>;
   return (
-    <div className="overflow-x-auto">
+    <div className={"overflow-x-auto" + (isLoading ? ' loading' : '')}>
       <table className="min-w-full border border-neutral-600">
         <thead className="bg-neutral-800 text-xs uppercase">
           <tr>
             <th className="px-3 py-2 text-left">#</th>
-            <th className="px-3 py-2 text-left">Prompt ID</th>
-            <th className="px-3 py-2">Workflow ID</th>
+            <th className="px-3 py-2 text-left">Name</th>
           </tr>
         </thead>
         <tbody>
-          {running.map(item => (
+          {state.running.map(item => (
             <QueueItemRow item={item} key={item[0]} />
           ))}
-          {pending.map(item => (
+          {state.pending.map(item => (
             <QueueItemRow item={item} key={item[0]} />
           ))}
         </tbody>
